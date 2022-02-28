3714a8a6cf1f90a25e6e7a635a2b7a48c94d0e42
Phil <znzpzp13@naver.com>
2022-02-28 23:53:04 +0900

    cloning

--- src/js/chat.js
+++ src/js/chat.js
@@ -7,10 +7,14 @@ const chatList = document.querySelector(".chatting-list")
 const chatInput = document.querySelector(".chatting-input")
 const sendButton = document.querySelector(".send-button")
 
+sendButton.addEventListener("click", (e) => {
+
+},false)
+
 socket.emit("chatting", "from front")
 
 socket.on("chatting", (data)=>{
-    console.log(data)
+    //console.log(data)
 })
 
 console.log(socket)
